0375ccbb1be2803054dcc1534226076832114b25
GRgithub <44814228+GRdesigning@users.noreply.github.com>
2018-11-06 12:37:56 -0500

    Update gptransfer.sh

---
+++ gptransfer.sh
@@ -0,0 +1,3 @@
+time gptransfer -t analytics_prod.analytics_wellcare_monthly19.output_combined_table_measure  schema_file   --source-port 5432 \
+ --source-host rsgpdb1-ext.medassurant.local  --source-user gpadmin --dest-user gpadmin --dest-port 5432 \
+   --dest-host rstagpdb01.medassurant.local  --dest-database analytics_wellcare  --source-map-file /home/gpadmin/source-map -a > gptransferoctm.out 2>&1
